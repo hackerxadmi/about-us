be41e07f1bc768a4bf4a756bdfe663e3d160f013
hackerxadmi <82869355+hackerxadmi@users.noreply.github.com>
2021-04-21 14:53:54 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+sleep 1
+figlet HackerX
+sleep 2
+echo "▇◤▔▔▔▔▔▔▔◥▇"
+echo "▇▏◥▇◣┊◢▇◤▕▇"
+echo "▇▏▃▆▅▎▅▆▃▕▇"
+echo "▇▏╱▔▕▎▔▔╲▕▇"
+echo "▇◣◣▃▅▎▅▃◢◢▇"
+echo "▇▇◣◥▅▅▅◤◢▇▇"
+echo "▇▇▇◣╲▇╱◢▇▇▇"
+echo "▇▇▇▇◣▇◢▇▇▇▇"
+echo "▇▇▇▇◣▇◢▇▇▇▇"
+echo "==WE ARE ANONYMOUS==";
+sleep 2
+echo "Author:Anonymous"
+echo "Email:nazimcp@gmail.com"
+echo "Telegram:@hacxbot"
+echo "Github:Nazimcp"
+echo "Website:www.hackerxapps.blogspot.com";
+sleep 1
+echo "▄︻┻═┳一 🔴🔴--ЩєLc๏Mє ┼๏ ๏Цr ┼єrmЦx Pągє HącKєrX--🔴🔴"
+echo "▄︻┻═┳一 🔴--------♥️HącKєrX-๏FFIcIąL♥️---------🔴"
+echo "▄︻┻═┳一 🔴===💪💪===♥️♥️┼ђє Mąs┼єr ๏F HącKiNg♥️♥️===💪💪===🔴"
+echo "▄︻┻═┳一 🔴🔴----♥️♥️==HącKєrX==♥️♥️----🔴🔴"
+echo "▄︻┻═┳一 🔴🔴{========FєєL ┼ђє P๏Щєr========}🔴🔴"
+echo "▄︻┻═┳一 🔴-------♥️ЩєLc๏Mє ┼๏ HącKєrX♥️--------🔴";
+sleep 1
+echo "This is our termux offical page"
+sleep 1
+echo "About us"
+sleep 2
+echo "We are HackerX.We are Anonymous.We are mod makers.We carak 15 something applications and games.We are also a app developers.Our appliacations are avilable in our website(www.hackerxapps.blogspot.com).We have also a Telegram bot(@hacxbot).You can also ask to as your douts."
+sleep 3
+echo "So thanks for visting our officail termux page"
+echo "See you so bye"
+sleep 1
+figlet HackerX
